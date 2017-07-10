2cc890050800d745878749deb362169e569fa03d
Kharianne <katerinamrkackova@gmail.com>
2017-07-10 14:48:46 +0200

    name changes and moving url_params making inside find flight function

--- book_flight.py
+++ book_flight.py
@@ -7,9 +7,9 @@ passenger = ap.read_configuration('passenger.ini')
 passenger_dict = ap.prepare_passenger(passenger)
 parser = ap.Parser()
 values = parser.argument_parser()
-validator = ap.Validator(values)
+validator = ap.Validator(values, config)
 
-if validator.date_validator() is False:
+if validator.validate_date() is False:
     print("Invalid date format. Example: YYYY-MM-DD")
     exit(1)
 
@@ -17,9 +17,8 @@ if validator.is_date_in_future() is False:
     print("Date is in the past. Example: YYYY-MM-DD")
     exit(1)
 
-params = {'api_key': config['IATA_validation_api']['api_key'], 'code': values['from']}
 try:
-    validator.iata_validation(config['IATA_validation_api']['url'], params)
+    validator.iata_validation()
 except urllib3.exceptions.MaxRetryError:
     print("Max retries achieved - probably network error.")
     exit(1)
@@ -28,10 +27,9 @@ except Exception as e:
     exit(1)
 
 flight = ap.Flight(values)
-url_params = flight.flight_params_builder()
 
 try:
-    booking_token = flight.find_flight(url_params, config['Flight_api']['url'])
+    booking_token = flight.find_flight(config['Flight_api']['url'])
     if booking_token is False:
         print("No results found!")
         exit(1)
@@ -45,6 +43,3 @@ try:
 except urllib3.exceptions.MaxRetryError:
     print("Max retries achieved - probably network error.")
     exit(1)
-
-
-
